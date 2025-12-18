f273d2e37a136dbd40576892e649c94c2b5d0f39
alexvi <alexvi@4psa.me>
2025-12-18 14:59:39 +0000

    Fix telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,7 +3,7 @@ function plugins(use)
 	use "kyazdani42/nvim-tree.lua"
 	use "lewis6991/gitsigns.nvim"
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		'nvim-telescope/telescope.nvim', branch = 'master',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
